2f1ec89c296245d8bb0a1d6026cadcdcafdadda5
andre_stylianos <andre.stylianos@gmail.com>
2013-12-03 20:25:10 -0200

    fixed indendentation

--- mpc_lyrics.py
+++ mpc_lyrics.py
@@ -1,18 +1,19 @@
 import subprocess
 import shlex
 
+
 def now_playing():
-	cmd = 'mpc current'
-	music = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE)
-	playing = music.communicate()[0].decode().split("\n")
-	return playing
+    cmd = 'mpc current'
+    music = subprocess.Popen(shlex.split(cmd),stdout=subprocess.PIPE)
+    playing = music.communicate()[0].decode().split("\n")
+    return playing
 
 def display_lyr(music):
-	cmdcat = 'cat "/home/pantera/.lyrics/'+music[0]+'.txt"'
-	cmdcat = shlex.split(cmdcat)
-	cmddmenu = 'dmenu -l 40 -fn "inconsolata-13" -p "Lyrics" -nb "#2B0123" -nf white -sb white -sf "#2B0123"'
-	cmddmenu = shlex.split(cmddmenu)
-	p1 = subprocess.Popen(cmdcat, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	subprocess.Popen(cmddmenu,stdin=p1.stdout)
+    cmdcat = 'cat "/home/pantera/.lyrics/'+music[0]+'.txt"'
+    cmdcat = shlex.split(cmdcat)
+    cmddmenu = 'dmenu -l 40 -fn "inconsolata-13" -p "Lyrics" -nb "#2B0123" -nf white -sb white -sf "#2B0123"'
+    cmddmenu = shlex.split(cmddmenu)
+    p1 = subprocess.Popen(cmdcat, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    subprocess.Popen(cmddmenu,stdin=p1.stdout)
 
 display_lyr(now_playing())
--- xrandr_multiple_monitor.py
+++ xrandr_multiple_monitor.py
@@ -3,8 +3,8 @@ import re
 import shlex
 
 def split_cmd(cmd):
-	return cmd.split(" ")
-	
+    return cmd.split(" ")
+
 def get_output(cmd):
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
     out = process.communicate()[0].decode()
@@ -12,53 +12,53 @@ def get_output(cmd):
     return out
 
 def connected_monitor(xrandr):
-	m = []
-	for l in xrandr:
-		if ' connected' in l:
-			connected = l.split(" connected")
-			m.append(connected[0])
-	return m
-	
+    m = []
+    for l in xrandr:
+        if ' connected' in l:
+            connected = l.split(" connected")
+            m.append(connected[0])
+    return m
+
 def layout_opt(monitor):
-	dmenargs = 'printf "Built-in\nExternal\nExtend\nMirror"'
-	cmd1 = shlex.split(dmenargs)
-	dmenopt = 'dmenu -p "'+monitor[1]+' / '+monitor[0]+'" -i -nb "#2B0123" -nf white -sb white -sf "#2B0123" -fn red-13'
-	cmd2 = shlex.split(dmenopt)
-	printf = subprocess.Popen(cmd1, stdout=subprocess.PIPE)
-	dmenu = subprocess.Popen(cmd2, stdin=printf.stdout ,stdout=subprocess.PIPE)
-	chosen = dmenu.communicate()[0].decode()
-	print(chosen)
-	layouts = {'Built-in\n': builtin,
-	 'External\n': external,
-	 'Extend\n': extend,
-	 'Mirror\n': mirror}
-	layouts[chosen](monitor)
+    dmenargs = 'printf "Built-in\nExternal\nExtend\nMirror"'
+    cmd1 = shlex.split(dmenargs)
+    dmenopt = 'dmenu -p "'+monitor[1]+' / '+monitor[0]+'" -i -nb "#2B0123" -nf white -sb white -sf "#2B0123" -fn red-13'
+    cmd2 = shlex.split(dmenopt)
+    printf = subprocess.Popen(cmd1, stdout=subprocess.PIPE)
+    dmenu = subprocess.Popen(cmd2, stdin=printf.stdout ,stdout=subprocess.PIPE)
+    chosen = dmenu.communicate()[0].decode()
+    print(chosen)
+    layouts = {'Built-in\n': builtin,
+               'External\n': external,
+               'Extend\n': extend,
+               'Mirror\n': mirror}
+    layouts[chosen](monitor)
 
 def change_layout(config):
-	cmd = shlex.split(config)
-	subprocess.Popen(cmd)		
+    cmd = shlex.split(config)
+    subprocess.Popen(cmd)
 
 def builtin(monitor):
-	xrandrconfig = "xrandr --output "+monitor[1]+" --auto --output "+monitor[0]+" --off"
-	change_layout(xrandrconfig)
+    xrandrconfig = "xrandr --output "+monitor[1]+" --auto --output "+monitor[0]+" --off"
+    change_layout(xrandrconfig)
 
 def external(monitor):
-	xrandrconfig = "xrandr --output "+monitor[0]+" --auto --output "+monitor[1]+" --off"
-	change_layout(xrandrconfig)
-	
+    xrandrconfig = "xrandr --output "+monitor[0]+" --auto --output "+monitor[1]+" --off"
+    change_layout(xrandrconfig)
+
 def extend(monitor):
-	xrandrconfig = "xrandr --output "+monitor[1]+" --auto --primary --output "+monitor[0]+" --auto --left-of "+monitor[1]
-	change_layout(xrandrconfig)
-	
+    xrandrconfig = "xrandr --output "+monitor[1]+" --auto --primary --output "+monitor[0]+" --auto --left-of "+monitor[1]
+    change_layout(xrandrconfig)
+
 def mirror(monitor):
-	xrandrconfig = "xrandr --output "+monitor[1]+" --auto --output "+monitor[0]+" --auto --same-as "+monitor[1]
-	change_layout(xrandrconfig)
-	
+    xrandrconfig = "xrandr --output "+monitor[1]+" --auto --output "+monitor[0]+" --auto --same-as "+monitor[1]
+    change_layout(xrandrconfig)
+
 def main():
-	xrandrquery = get_output(split_cmd("xrandr -q")).split("\n")
-	monitor = connected_monitor(xrandrquery)
-	print(monitor)
-	layout_opt(monitor)
-	
-	
+    xrandrquery = get_output(split_cmd("xrandr -q")).split("\n")
+    monitor = connected_monitor(xrandrquery)
+    print(monitor)
+    layout_opt(monitor)
+
+
 main()
